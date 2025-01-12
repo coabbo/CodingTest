293cfeeda1e7857dd37fb8c140b5f1a595686fdd
Minju Jeong <coabbo@naver.com>
2025-01-12 19:53:25 +0900

    [D1] Title: 아주 간단한 계산기, Time: 136 ms, Memory: 20,256 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int num1;
+        int num2;
+		num1=sc.nextInt();
+        num2=sc.nextInt();
+        
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+	}
+}
