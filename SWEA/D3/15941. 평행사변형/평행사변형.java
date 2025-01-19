55d32772228aefc9041a5ad86ba6ce0fbf5415dc
Minju Jeong <coabbo@naver.com>
2025-01-19 23:23:10 +0900

    [D3] Title: 평행사변형, Time: 150 ms, Memory: 21,144 KB -BaekjoonHub

---
+++ SWEA/D3/15941. 평행사변형/평행사변형.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            System.out.print("#" + test_case + " ");
+            
+            int num = sc.nextInt();
+            
+            System.out.println(num * num);
+		}
+	}
+}
