0ef7f1ff8c060df17b27ec52a4db4e6610b59101
Minju Jeong <coabbo@naver.com>
2025-02-23 20:48:46 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 157 ms, Memory: 35,732 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+  
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  
+        for (int tc = 1; tc <= 10; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            List<Integer> code = new LinkedList<>();
+  
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                code.add(Integer.parseInt(st.nextToken()));
+            }
+  
+            int commandCount = Integer.parseInt(br.readLine());
+  
+            st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                String s = st.nextToken();
+                if (s.equals("I")) {
+                    int location = Integer.parseInt(st.nextToken()); // 위치
+                    int x = Integer.parseInt(st.nextToken()); // 개수
+                    for (int i = 0; i < x; i++) {
+                        int num = Integer.parseInt(st.nextToken());
+                        code.add(location + i, num);
+                    }
+                }
+                if (s.equals("D")) {
+                    int location = Integer.parseInt(st.nextToken()); // 위치
+                    int x = Integer.parseInt(st.nextToken()); // 개수
+                    for (int i = 0; i < x; i++) {
+                        code.remove(location + i);
+                    }
+                }
+                if (s.equals("A")) {
+                    int x = Integer.parseInt(st.nextToken()); // 개수
+                    for (int i = 0; i < x; i++) {
+                        code.add(Integer.parseInt(st.nextToken()));
+                    }
+                }
+            }
+  
+            StringBuilder sb = new StringBuilder();
+            sb.append("#").append(tc).append(" ");
+            for (int i = 0; i < 10; i++) {
+                sb.append(code.get(i)).append(" ");
+            }
+            System.out.println(sb.toString());
+        }
+    }
+}
