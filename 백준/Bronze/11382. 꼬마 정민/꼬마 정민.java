68c98328fea1d6fb56def1ed3ab7b3b472972e08
Minju Jeong <coabbo@naver.com>
2025-02-24 23:12:32 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		long C = Long.parseLong(st.nextToken());
+		
+		long answer = A + B + C;
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(answer);
+		System.out.println(sb.toString());
+	}
+}
