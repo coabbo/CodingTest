025a15b5159484f05ca509543be15a96637d410d
Minju Jeong <coabbo@naver.com>
2025-02-26 14:50:26 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 228 ms, Memory: 19816 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = N-i; j >= 1; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
