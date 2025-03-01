b125dfa9f8fef9a33358112233fa1f2ef8fe40eb
Minju Jeong <coabbo@naver.com>
2025-03-01 23:49:08 +0900

    [Bronze I] Title: 단어 공부, Time: 468 ms, Memory: 88208 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine().toUpperCase();
+		
+		Map<String, Integer> inputMap = new HashMap<>();
+		for (int i = 0; i < input.length(); i++) {
+			String key = "" + input.charAt(i);
+			if(!inputMap.containsKey(key)) {
+				inputMap.put(key, 0);
+			}
+			else {
+				inputMap.put(key, inputMap.get(key)+1);
+			}
+		}
+		
+		int max = 0;
+		for (String key : inputMap.keySet()) {
+			int num = inputMap.get(key);
+			max = Math.max(num, max);
+		}
+		
+		String answer = "";
+		int count = 0;
+		for (String key : inputMap.keySet()) {
+			int num = inputMap.get(key);
+			if(num == max) {
+				answer = key;
+				count += 1;
+			}
+		}
+		
+		if(count > 1) {
+			System.out.println("?");
+		} else {
+			System.out.println(answer);
+		}
+	}
+}
