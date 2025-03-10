d0a90f995d6fe3eec4ccb9eb3966dffb77eb59e3
Minju Jeong <coabbo@naver.com>
2025-03-10 17:53:40 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1224 ms, Memory: 59652 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] xy = new int[N][2];
+		
+		int idx = 0;
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			xy[idx][0] = Integer.parseInt(st.nextToken());
+			xy[idx][1] = Integer.parseInt(st.nextToken());
+			
+			idx++;
+		}
+		
+		Arrays.sort(xy, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0]!=o2[0] ? o1[0]-o2[0] : o1[1]-o2[1]; // 첫번째 기준 오름차순 > 두번째 기준 오름차
+			}
+		});
+		
+		for (int i = 0; i < xy.length; i++) {
+			System.out.println(xy[i][0] + " " + xy[i][1]);
+		}
+	}
+}
