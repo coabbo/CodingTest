8046dc1c712b7218a2df3fcabee62f5f89f385cd
Minju Jeong <coabbo@naver.com>
2025-03-12 12:04:34 +0900

    [Silver V] Title: 숫자 카드, Time: 2672 ms, Memory: 159108 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,50 +9,23 @@ public class Main {
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] sanggenCard = new int[N];
+		Set<Integer> sanggenCardSet = new HashSet<>();
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < N; i++) {
-			sanggenCard[i] = Integer.parseInt(st.nextToken());
+			sanggenCardSet.add(Integer.parseInt(st.nextToken()));
 		}
 		
 		int M = Integer.parseInt(br.readLine());
-		int[] numCard = new int[M];
 		
 		st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < M; i++) {
-			numCard[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		// 이분탐색은 정렬된 배열에서만 올바르게 동작하기 때문에 정렬을 수행함
-		Arrays.sort(sanggenCard);
-		
-		// 이분탐색 
-		for (int i = 0; i < numCard.length; i++) {
-			int findNum = numCard[i];
-			
-			
-			int start = 0;
-			int end = sanggenCard.length - 1; 
-					
-			int isHave = 0;
-			
-			while(start <= end) {
-				int mid = (start + end) / 2;
-				
-				if(findNum < sanggenCard[mid]) {
-					end = mid - 1;
-				}
-				else if(findNum > sanggenCard[mid]) {
-					start = mid + 1;
-				}
-				else {
-					isHave = 1;
-					break;
-				}
+			if(sanggenCardSet.contains(Integer.parseInt(st.nextToken()))){
+				System.out.print("1 ");
 			}
-			
-			System.out.print(isHave + " ");
+			else {
+				System.out.print("0 ");
+			}
 		}
 	}
 }
