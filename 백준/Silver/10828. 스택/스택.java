193da4f2ab5ef0d8f9affe55301a017936b533d2
Minju Jeong <coabbo@naver.com>
2025-03-13 21:40:07 +0900

    [Silver IV] Title: 스택, Time: 160 ms, Memory: 20112 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,21 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
 		Stack<Integer> stack = new Stack<>();
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		
 		for (int i = 0; i < N; i++) {
-			String s = sc.next();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
 
 			if(s.equals("push")) {
-				stack.push(sc.nextInt());
+				stack.push(Integer.parseInt(st.nextToken()));
 			} 
 			else if(s.equals("top")) {
 				if(stack.size() == 0) {
