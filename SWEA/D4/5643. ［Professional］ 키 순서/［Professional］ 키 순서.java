0ea8d6785da44d5af6810755b3d33fa4d2d12f6d
Minju Jeong <coabbo@naver.com>
2025-04-02 16:56:24 +0900

    [D4] Title: [Professional] 키 순서, Time: 362 ms, Memory: 98,592 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -3,7 +3,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * 플로이드 워샬
+ * dfs, 메모이제이션
+ * 나보다 큰 부모를 찾아가서, 부모보다 큰 사람의 정보를 가져와서 내꺼에 업데이트
  */
 public class Solution {
 	
@@ -18,12 +19,6 @@ public class Solution {
 			int N = Integer.parseInt(br.readLine()); // 학생들 수, 2 <= N <= 500
 			int M = Integer.parseInt(br.readLine()); // 비교 횟수, 0 <= M <= N*(N-1)/2
 			a = new int[N+1][N+1]; // 0번 안 씀, 인접행렬
-//			인접하지 않은 관계를 표시하기 위해 큰 값으로 초기화
-			for (int i = 1; i < a.length; i++) {
-				for (int j = 1; j < a.length; j++) {
-					a[i][j] = 500;
-				}
-			}
 			
 			for (int i = 0; i < M; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -31,40 +26,39 @@ public class Solution {
 				int tall = Integer.parseInt(st.nextToken());
 				a[small][tall] = 1; // 단방향
 			}
-			
-			for (int k = 1; k <= N; k++) { // 경유지
-				for (int start = 1; start <= N; start++) { // 출발지
-					if (k == start) continue; // 위 입력 시 if (i == j) map[i][j] = 0; 인 경우 코드를 생략할 수 있음, 생략해야 더 빠름
-					for (int end = 1; end <= N; end++) { // 도착지
-						if (k == end || start == end) continue; // 위 입력 시 if (i == j) map[i][j] = 0; 인 경우 코드를 생략할 수 있음, 생략해야 더 빠름
-						if (a[start][end] > a[start][k] + a[k][end]) {
-							a[start][end] = a[start][k] + a[k][end];
-						}
-//						a[start][end] = Math.min(a[start][k] + a[k][end], a[start][end]); // 빠름
-					}
-				}
+		
+			for (int i = 0; i < a.length; i++) {
+				up(i); // 정점보다 큰 정점들 탐색하면서 인접행렬 업데이트
 			}
 			
+			int cnt = 0; // 자신의 순위를 정확히 알 수 있는 학ㄷ생 수
 			for (int i = 1; i < a.length; i++) {
+				int sum = 0; // 나보다 큰 친구, 작은 친구를 누적
 				for (int j = 1; j < a.length; j++) {
-					if (a[j][i] < 500) a[0][i]++;
-					if (a[i][j] < 500) a[i][0]++;
+					sum += a[i][j] + a[j][i];
 				}
-			}
-			
-			int cnt = 0; // 자신의 순위를 정확히 알 수 있는 학생 수
-			for (int i = 1; i < a.length; i++) {
-				int sum = 0;
-				for (int j = 1; j < a.length; j++) {
-					if (a[j][i] < 500) sum++;
-					if (a[i][j] < 500) sum++;
-				}
-				if (sum == N-1) { // 자신의 순위를 정확히 알 수 있으면 N-1
+				if (sum == N-1) { // 자신의 순위를 정확하게 알 수 있음
 					cnt++;
 				}
 			}
+			
 			sb.append("#").append(testCase).append(" ").append(cnt).append("\n");
 		} // end of for testCase
 		System.out.println(sb.toString());
 	} // end of main
+
+
+	/** 나보다 큰 부모를 찾아가서, 부모보다 큰 사람의 정보를 가져와서 내꺼에 업데이트 */
+	private static void up(int v) {
+		if (a[v][0] == -1) return; // 작업완료 됨
+		for (int i = 1; i < a.length; i++) {
+			if(a[v][i] == 1) { // 나보다 큼
+				up(i); //부모꺼는 업데이트 완료
+				for (int j = 0; j < a.length; j++) { // 부모보다 큰 친구들을 내꺼에 업데이트
+					a[v][j] |= a[i][j]; 
+				}
+			}
+		}
+		a[v][0] = -1; // 작업 완료 표시
+	}
 } // end of class
