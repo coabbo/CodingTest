0c6877072ef3d14e603641ba3ab2465d099bc5d4
Minju Jeong <coabbo@naver.com>
2025-04-03 12:11:22 +0900

    [D2] Title: 쉬운 거스름돈, Time: 80 ms, Memory: 24,576 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,46 +1,30 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-class Solution
-{
-	public static int returnCount(int num, int moneyUnit) {
-		int count;
-        count = num / moneyUnit;
-        num = num % moneyUnit;
-        System.out.print(count + " ");
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
-		return num;
-	}
-	
-	public static void main(String args[]) throws Exception
-	{   
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-        int count50000;
-        int count10000;
-        int count5000;
-        int count1000;
-        int count500;
-        int count100;
-        int count50;
-        int count10;
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            System.out.println("#" + test_case);
-            
-            int num = sc.nextInt();
-            
-            num = returnCount(num, 50000);
-            num = returnCount(num, 10000);
-            num = returnCount(num, 5000);
-            num = returnCount(num, 1000);
-            num = returnCount(num, 500);
-            num = returnCount(num, 100);
-            num = returnCount(num, 50);
-            num = returnCount(num, 10);
-
-            System.out.println("");
+		int[] coins = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+		int[] answer = new int[8];
+		
+		int T = Integer.parseInt(br.readLine()); //테스트케이스 개수
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			sb.append("#").append(testCase).append(" ").append("\n");
+			
+			int money = Integer.parseInt(br.readLine());
+			
+			for (int i = 0; i < answer.length; i++) {
+				answer[i] = money / coins[i];
+				sb.append(answer[i]).append(" ");
+				money = money % coins[i];
+			}
+			
+			sb.append("\n");
 		}
+		
+		System.out.println(sb.toString());
 	}
 }
