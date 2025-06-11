8ad9e1961a883d60698f52b6e438e0e05dbb15ea
Minju Jeong <coabbo@naver.com>
2025-06-11 18:26:14 +0900

    [Silver IV] Title: 수열, Time: 240 ms, Memory: 23576 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.java
+++ 백준/Silver/2491. 수열/수열.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,42 +14,28 @@ public class Main {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int answer = 0;
-		int count = 0;
+		int answer = 1;
 		int plusLen = 1;
 		int minusLen = 1;
 		
-		for (int i = 0; i < arr.length; i++) {
-			
+		for (int i = 1; i < N; i++) {
 			// 연속해서 커지는 수열의 길이 체크
-			int temp = arr[i];
-			for (int j = i+1; j < arr.length; j++) {
-				if(temp <= arr[j]) {
-					plusLen++;
-					temp = arr[j];
-				} 
-				else {
-					break;
-				}
+			if (arr[i-1] <= arr[i]) {
+				plusLen++;
+			}
+			else {
+				plusLen = 1;
 			}
 			
 			// 연속해서 작아지는 수열의 길이 체크
-			temp = arr[i];
-			for (int j = i+1; j < arr.length; j++) {
-				if(temp >= arr[j]) {
-					minusLen++;
-					temp = arr[j];
-				} 
-				else {
-					break;
-				}
+			if(arr[i-1] >= arr[i]) {
+				minusLen++;
+			}
+			else {
+				minusLen = 1;
 			}
 			
-			count = Math.max(plusLen, minusLen);
-			answer = Math.max(answer, count);
-			
-			plusLen = 1;
-			minusLen = 1;
+			answer = Math.max(answer, Math.max(plusLen, minusLen));
 		}
 		
 		System.out.println(answer);
