db40407e87d418a7538b5d5e0e65fa7f12e7d7f4
Minju Jeong <coabbo@naver.com>
2025-06-23 19:01:31 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 144 ms, Memory: 15736 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,43 +13,20 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
-		int[] arr = new int[N+1];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = i;
-		}
-		
-		List<Integer> list = new ArrayList<Integer>();
-		list.add(arr[K]);
-		arr[K] = 0;
-		int count = 0;
-		int curr = K;
-		
-		while(true) {
-			if(list.size() == arr.length - 1) {
-				break;
-			}
-			
-			curr = (curr + 1) % (N + 1);
-			if(arr[curr] != 0) {
-				count++;
-				
-				if(count == K) {
-					list.add(arr[curr]);
-					arr[curr] = 0;
-					count = 0;
-				}
-			}
+		Deque<Integer> q = new ArrayDeque<>();
+		for (int i = 1; i <= N; i++) {
+			q.offer(i);
 		}
 		
 		sb.append("<");
-		for (int i = 0; i < list.size(); i++) {
-			if(i != list.size() - 1) {
-				sb.append(list.get(i)).append(", ");
+		while(!q.isEmpty()) {
+			for (int i = 1; i < K; i++) {
+				q.offer(q.poll());
 			}
-			else {
-				sb.append(list.get(i)).append(">");
-			}
+			sb.append(q.poll());
+			if(!q.isEmpty()) sb.append(", ");
 		}
+		sb.append(">");
 		
 		System.out.println(sb.toString());
 	}
