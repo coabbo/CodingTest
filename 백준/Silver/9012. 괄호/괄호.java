3931ee2819f2cf062c99bda148ba8e13fcabdc3c
Minju Jeong <coabbo@naver.com>
2025-06-23 12:18:04 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine()); // 테스트케이스 개수
+		
+		for (int testCase = 0; testCase < T; testCase++) {
+			String s = br.readLine();
+			
+			Stack<String> stack = new Stack<String>();
+				
+			for (int i = 0; i < s.length(); i++) {
+				// 왼쪽 괄호면 push
+				if(s.charAt(i) == '(') stack.push("(");
+				if(s.charAt(i) == '[') stack.push("[");
+					
+				// 오른쪽 괄호면 상황에 따라 pop or push
+				if(s.charAt(i) == ')') {
+					if(!stack.isEmpty()) {
+						if(stack.peek().equals("(")) stack.pop();
+						else stack.push(")");
+					}
+					else stack.push(")");
+				}
+				if(s.charAt(i) == ']') {
+					if(!stack.isEmpty()) {
+						if(stack.peek().equals("[")) stack.pop();
+						else stack.push("]");
+					}
+					else stack.push("]");
+				}
+			}
+				
+			if(stack.isEmpty()) sb.append("YES").append("\n");
+			else sb.append("NO").append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
