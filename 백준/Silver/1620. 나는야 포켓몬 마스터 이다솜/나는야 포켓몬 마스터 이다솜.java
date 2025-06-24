626907ee59d8a05b79208dc8454a7e45a381a309
Minju Jeong <coabbo@naver.com>
2025-06-24 11:42:40 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 508 ms, Memory: 58796 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken()); // 포켓몬의 개수
+		int M = Integer.parseInt(st.nextToken()); // 문제의 개수
+
+		HashMap<Integer, String> indexToNameMap = new HashMap<Integer, String>();
+		HashMap<String, Integer> NameToIndexMap = new HashMap<String, Integer>();
+		
+		for (int i = 1; i < N+1; i++) {
+			String name = br.readLine();
+			indexToNameMap.put(i, name);
+			NameToIndexMap.put(name, i);
+		}
+		
+		for (int i = 0; i < M; i++) {
+			String s = br.readLine();
+			
+			if(s.charAt(0) >= '0' && s.charAt(0) <= '9') {
+				int index = Integer.parseInt(s);
+				sb.append(indexToNameMap.get(index)).append("\n");
+			}
+			else {
+				sb.append(NameToIndexMap.get(s)).append("\n");
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
