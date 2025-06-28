58616bcfc31e07b4d110ae66d55e502bcddb905f
Minju Jeong <coabbo@naver.com>
2025-06-28 14:54:58 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 20796 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,73 +1,50 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	private static int N;
-	private static int K;
-	private static List<Integer>[] graph;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken()); // 수빈이가 있는 위치
-		K = Integer.parseInt(st.nextToken()); // 동생이 있는 위치
+		int N = Integer.parseInt(st.nextToken()); // 수빈이가 있는 위치
+		int K = Integer.parseInt(st.nextToken()); // 동생이 있는 위치
 		
-		graph = new ArrayList[100000 + 1];
-		for (int i = 0; i <= 100000; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		
-		// 간선 입력
-		for (int i = 0; i <= 100000; i++) {
-			if(i == 0) {
-				graph[i].add(i+1);
-				graph[i].add(i*2);
-			}
-			else {
-				graph[i].add(i-1);
-				graph[i].add(i+1);
-				graph[i].add(i*2);
-			}
-		}
-		
-		int ans = bfs(N);
+		int ans = bfs(N, K);
 		System.out.println(ans);
 	}
 
-	public static int bfs(int start) {
+	public static int bfs(int start, int target) {
 		Queue<Integer> q = new ArrayDeque<>();
 		q.add(start);
 		
-		int[] second = new int[100000 + 1];
-		Arrays.fill(second, -1);
-		second[start] = 0;
+		boolean[] visited = new boolean[100000 + 1];
+		visited[start] = true;
 		
-		int minSecond = Integer.MAX_VALUE;
+		int[] dist = new int[100000 + 1];
+		dist[start] = 0;
+		
 		while(!q.isEmpty()) {
 			int curr = q.poll();
 			
-			if(curr == K) {
-				minSecond = second[K];
-				break;
+			if(curr == target) {
+				return dist[curr];
 			}
 			
-			for (int v : graph[curr]) {
-				if(v < 0 || v > 100000) continue;
-				
-				if(second[v] == -1 && (v >= 0 && v <= 100000)) {
+			// 이동할 수 있는 세 가지 경우
+			int[] nexts = {curr - 1, curr + 1, curr * 2};
+			
+			for (int v : nexts) {
+				if(v >= 0 && v <= 100000 && !visited[v]) {
 					q.add(v);
-					second[v] = second[curr] + 1;
+					visited[v] = true;
+					dist[v] = dist[curr] + 1;
 				}
 			}
 		}
 		
-		return minSecond;
+		return -1;
 	}
 }
