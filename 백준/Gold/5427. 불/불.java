ee4a9cbeaa848572062d6d38f34ae847d5f35c10
Minju Jeong <coabbo@naver.com>
2025-08-19 17:00:42 +0900

    [Gold IV] Title: 불, Time: 588 ms, Memory: 85824 KB -BaekjoonHub

---
+++ 백준/Gold/5427. 불/불.java
@@ -0,0 +1,118 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static int W, H;
+    private static int fireCount;
+    private static char[][] map;
+    private static int[][] fireDist;
+    private static int[][] jihunDist;
+
+    private static int[] dr = {-1, 1, 0, 0};
+    private static int[] dc = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int testCase = 0; testCase < T; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            W = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken());
+
+            map = new char[H][W];
+            int sanggeunR = 0;
+            int sanggeunC = 0;
+
+            for (int i = 0; i < H; i++) {
+                String s = br.readLine();
+                for (int j = 0; j < W; j++) {
+                    map[i][j] = s.charAt(j);
+
+                    if(s.charAt(j) == '@'){
+                        sanggeunR = i;
+                        sanggeunC = j;
+                    }
+                }
+            }
+
+            fireDist = new int[H][W];
+            for (int i = 0; i < H; i++) {
+                Arrays.fill(fireDist[i], -1);
+            }
+            bfsFire();
+
+            jihunDist = new int[H][W];
+            for (int i = 0; i < H; i++) {
+                Arrays.fill(jihunDist[i], -1);
+            }
+            bfsJihun(sanggeunR, sanggeunC);
+        }
+    }
+
+    public static void bfsFire() {
+        Queue<int[]> q = new ArrayDeque<>();
+
+        for (int r = 0; r < H; r++) {
+            for (int c = 0; c < W; c++) {
+                if(map[r][c] == '*') {
+                    fireCount++;
+                    q.add(new int[] {r, c});
+                    fireDist[r][c] = 0;
+                }
+                if(map[r][c] == '#') fireDist[r][c] = -100;
+            }
+        }
+
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int cr = curr[0];
+            int cc = curr[1];
+
+            for (int d = 0; d < 4; d++) {
+                int nr = cr + dr[d];
+                int nc = cc + dc[d];
+
+                if(nr < 0 || nc < 0 || nr >= H || nc >= W) continue;
+                if(fireDist[nr][nc] == -100 || fireDist[nr][nc] != -1) continue;
+
+                q.add(new int[] {nr, nc});
+                fireDist[nr][nc] = fireDist[cr][cc] + 1;
+            }
+        }
+    }
+
+    private static void bfsJihun(int startR, int startC) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {startR, startC});
+        jihunDist[startR][startC] = 0;
+
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int cr = curr[0];
+            int cc = curr[1];
+
+            if(cr == 0 || cr == H-1 || cc == 0 || cc == W-1){
+                System.out.println(jihunDist[cr][cc] + 1);
+                return;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nr = cr + dr[d];
+                int nc = cc + dc[d];
+
+                if(nr < 0 || nc < 0 || nr >= H || nc >= W) continue;
+                if(fireDist[nr][nc] == -100 || jihunDist[nr][nc] != -1) continue;
+
+                if(map[nr][nc] == '.'){
+                    jihunDist[nr][nc] = jihunDist[cr][cc] + 1;
+
+                    if(fireDist[nr][nc] == -1 || jihunDist[nr][nc] < fireDist[nr][nc]) q.add(new int[] {nr, nc});
+                }
+            }
+        }
+
+        System.out.println("IMPOSSIBLE");
+    }
+}
