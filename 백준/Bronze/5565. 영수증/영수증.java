c698b97c90198e5922c24f3f6c534812561b241c
Minju Jeong <coabbo@naver.com>
2025-09-08 17:12:01 +0900

    [Bronze IV] Title: 영수증, Time: 64 ms, Memory: 11420 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 10권의 총 가격
+
+        for (int i = 0; i < 9; i++) {
+            N =  N - Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(N);
+    }
+}
