9f870f49c3f61d0c9275c8607ca8487baf2efeea
Minju Jeong <coabbo@naver.com>
2025-11-19 23:38:39 +0900

    [Bronze IV] Title: Practice: Roll Call, Time: 68 ms, Memory: 12372 KB -BaekjoonHub

---
+++ 백준/Bronze/9306. Practice： Roll Call/Practice： Roll Call.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= T; i++) {
+			String First = br.readLine();
+			String Last = br.readLine();
+			
+			sb.append("Case " + i + ": " + Last + ", " + First).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
