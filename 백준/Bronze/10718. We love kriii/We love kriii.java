4b1096be7d35b9d16d6cfb3c85f370da017e9409
Minju Jeong <coabbo@naver.com>
2025-11-20 23:10:05 +0900

    [Bronze V] Title: We love kriii, Time: 60 ms, Memory: 11332 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("강한친구 대한육군").append("\n");
+		sb.append("강한친구 대한육군").append("\n");
+		
+		System.out.println(sb.toString());
+	}
+}
