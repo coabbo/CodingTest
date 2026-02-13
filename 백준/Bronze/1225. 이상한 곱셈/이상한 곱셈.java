3d1d7e1c8960c7b5985d55320212ba90751612d6
Minju Jeong <coabbo@naver.com>
2026-02-14 00:36:25 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 200 ms, Memory: 11844 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		String A = st.nextToken();
+		String B = st.nextToken();
+		
+		long answer = 0;
+		for (int i = 0; i < A.length(); i++) {
+			for (int j = 0; j < B.length(); j++) {
+				answer += (A.charAt(i) - '0') * (B.charAt(j) - '0');
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
