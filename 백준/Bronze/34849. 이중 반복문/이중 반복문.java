48d335eb54ee4682e588e59b43a395781f423bcb
Minju Jeong <coabbo@naver.com>
2026-03-10 19:39:27 +0900

    [Bronze V] Title: 이중 반복문, Time: 68 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		
+		if (N <= 10000) System.out.println("Accepted");
+		else System.out.println("Time limit exceeded");
+	}
+}
