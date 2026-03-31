38860332efada84e57a11917e9771302211dccba
Minju Jeong <coabbo@naver.com>
2026-03-31 20:23:53 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 156 ms, Memory: 20504 KB -BaekjoonHub

---
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String N = br.readLine();
+		long n = 20000303;
+		
+		long answer = 0;
+		for (int i = 0; i < N.length(); i++) {
+			answer = (answer * 10 + (N.charAt(i) - '0')) % n;
+		}
+		
+		System.out.println(answer);
+	}
+}
