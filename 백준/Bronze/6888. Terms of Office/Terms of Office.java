8a37a4bfe91ac5d42034ab309c4829f220476651
Minju Jeong <coabbo@naver.com>
2026-04-09 23:44:40 +0900

    [Bronze IV] Title: Terms of Office, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+		int Y = Integer.parseInt(br.readLine());
+		
+		for (int i = X; i <= Y; i += 60) {
+			System.out.println("All positions change in year " + i);
+		}
+	}
+}
