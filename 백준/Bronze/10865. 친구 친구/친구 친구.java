039754e687cbfc51829ea013423645f86ad1cda0
Minju Jeong <coabbo@naver.com>
2026-04-12 01:31:36 +0900

    [Bronze III] Title: 친구 친구, Time: 520 ms, Memory: 211100 KB -BaekjoonHub

---
+++ 백준/Bronze/10865. 친구 친구/친구 친구.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			arr[A]++;
+			arr[B]++;
+		}
+		
+		for (int i = 1; i < arr.length; i++) {
+			sb.append(arr[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
